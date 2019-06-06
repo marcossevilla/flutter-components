6078dcb40e7b2461fcd8bd7496ad6dfed0a00f34
Marcos Sevilla <marcossevilla14@gmail.com>
2019-06-05 23:01:31 -0600

    temporal home page

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:components/src/pages/home_temp.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Components App',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Components'),
+          ),
+          body: HomePageTemp()),
+    );
+  }
+}
