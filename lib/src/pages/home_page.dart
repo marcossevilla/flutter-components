c557b309209eb7be254ffa38f4256a53407388a0
Marcos Sevilla <marcossevilla14@gmail.com>
2019-06-07 12:54:50 -0600

    get icon method for dynamic icons

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:components/src/providers/menu_provider.dart';
+import 'package:components/src/utils/icon_string_util.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -32,10 +33,7 @@ class HomePage extends StatelessWidget {
     data.forEach((op) {
       final widgetTemp = ListTile(
         title: Text(op['text']),
-        leading: Icon(
-          Icons.account_circle,
-          color: Colors.blue,
-        ),
+        leading: getIcon(op['icon']),
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
         onTap: () {},
       );
