2fe8e13028d7d051639ff1f8515492e4e23fca80
Marcos Sevilla <marcossevilla14@gmail.com>
2019-06-07 12:29:10 -0600

    add future builder and read text from json

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -13,19 +13,36 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _list() {
-    print(menuProvider.options);
-
-    return ListView(
-      children: _listItems(),
+    return FutureBuilder(
+      future: menuProvider.loadData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        print('builder');
+        print(snapshot.data);
+        return ListView(
+          children: _listItems(snapshot.data),
+        );
+      },
     );
   }
 
-  List<Widget> _listItems() {
-    return [
-      ListTile(
-        title: Text('Hello world!'),
-      ),
-      Divider(),
-    ];
+  List<Widget> _listItems(List<dynamic> data) {
+    final List<Widget> options = [];
+
+    data.forEach((op) {
+      final widgetTemp = ListTile(
+        title: Text(op['text']),
+        leading: Icon(
+          Icons.account_circle,
+          color: Colors.blue,
+        ),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
+        onTap: () {},
+      );
+
+      options..add(widgetTemp)..add(Divider());
+    });
+
+    return options;
   }
 }
--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/services.dart' show rootBundle;
 
 class _MenuProvider {
@@ -7,10 +9,13 @@ class _MenuProvider {
     loadData();
   }
 
-  loadData() {
-    rootBundle.loadString('data/menu_opts.json').then((data) {
-      print(data);
-    });
+  Future<List<dynamic>> loadData() async {
+    final res = await rootBundle.loadString('data/menu_opts.json');
+    Map dataMap = json.decode(res);
+    print('loadData: ');
+    print(dataMap['routes']);
+    options = dataMap['routes'];
+    return options;
   }
 }
 
